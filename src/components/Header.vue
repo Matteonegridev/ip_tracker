874de5b21aab4692277e47c7777e4606c50a0650
Matteo <matteonegridev@gmail.com>
2025-06-25 22:43:07 +0200

    added map + header

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <header>
+    <p></p>
+  </header>
+</template>
+
+<style lang="css" scoped>
+header {
+  background: url("../assets/images/pattern-bg-mobile.png");
+  background-size: 100%;
+  height: 250px;
+  box-shadow: 0 8px 20px rgba(0, 0, 0, 0.3);
+}
+
+@media screen and (min-width: 25rem) {
+  header {
+    background: url("../assets/images/pattern-bg-desktop.png");
+    background-size: 100%;
+    height: 250px;
+    box-shadow: 0 8px 20px rgba(0, 0, 0, 0.3);
+  }
+}
+</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,34 @@
+<script setup>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import { reactive, ref } from "vue";
+
+const zoom = ref(4);
+
+const coordinates = reactive({
+  x: 47.41322,
+  y: -1.219482,
+});
+</script>
+
+<template>
+  <div id="map">
+    <l-map ref="map" v-model:zoom="zoom" :center="[coordinates.x, coordinates.y]">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      ></l-tile-layer>
+    </l-map>
+  </div>
+</template>
+
+<style lang="css">
+#map {
+  display: block;
+  height: 100vh;
+  width: 100%;
+  position: relative;
+  z-index: -1;
+}
+</style>
